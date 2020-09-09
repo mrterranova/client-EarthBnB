e51a7e60bc347ce7bf9f013fd13351534e1e666a
JC <avalojc@gmail.com>
2020-09-09 14:44:48 -0400

    correct source issue wiht imgs, add source to profile image, create function for join date

--- src/app/hostmod/hostheader/hostheader.component.ts
+++ src/app/hostmod/hostheader/hostheader.component.ts
@@ -9,7 +9,25 @@ import { Data } from '@angular/router';
 })
 export class HostheaderComponent implements OnInit {
   @Input('hostdata') hostData: Data; // tslint:disable-line: no-input-rename
-  constructor() { }
+
+  julianParseFunction(excelDate:number): string {
+    //array of months to return month
+    var months:string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    //converts database date to epoch time Jan. 1, 1970
+    let toEpoch: number = (excelDate-25568)*86400*1000;
+    //converts toEpoch:number to a date.
+    let newDate = new Date(toEpoch)
+    //return specific month from months array by using getMonth method on newDate var
+    let month = months[newDate.getMonth()]
+    //return year from newDate
+    let year = newDate.getFullYear()
+    //returns what the month year in a string
+    let sentence:string = month+" "+year
+    return sentence
+  }
+
+  constructor() { 
+  }
 
   ngOnInit(): void {
   }
