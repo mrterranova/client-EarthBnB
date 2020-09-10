d94fc0634b1c842568e36e920182eee6b295f0f3
Adam Oliveras TP Rig <theadamoliveras@gmail.com>
2020-09-10 07:00:25 -0700

    Created initial files and folders for the listingInfo module of the application. Basic placeholders for now in the app component html file. Will start to flesh out the components next.

---
+++ src/app/listingInfo/amenities-info/amenities-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-amenities-info',
+  templateUrl: './amenities-info.component.html',
+  styleUrls: ['./amenities-info.component.css']
+})
+export class AmenitiesInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/listingInfo/listing-info-container/listing-info-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-info-container',
+  templateUrl: './listing-info-container.component.html',
+  styleUrls: ['./listing-info-container.component.css']
+})
+export class ListingInfoContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
