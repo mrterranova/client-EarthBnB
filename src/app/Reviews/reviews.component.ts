fead95784b67643772a2a126120c77b2deecbd11
Trevis42 <sirtrevis42@gmail.com>
2020-09-11 12:50:43 -0500

    updated component service import
    added --open=true to ng serve command

--- src/app/Reviews/reviews.component.ts
+++ src/app/Reviews/reviews.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ReviewsService } from '../services/ReviewsService/reviews.service';
+import { ReviewsService } from '../services/reviews.service';
 import { Data } from '@angular/router';
 
 @Component({
