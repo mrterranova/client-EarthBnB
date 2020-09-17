a01310c74b05d062b6a4bb5f06d694fa4679614d
Trevis42 <sirtrevis42@gmail.com>
2020-09-17 14:19:20 -0500

    added route redirect to first location

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router'
+import { Routes, RouterModule } from '@angular/router';
 import { DirectoryComponent } from '../directory/directory.component';
 
-
 const routes: Routes = [
-    {
-      path:"rooms/:id",
-      component: DirectoryComponent
-    }
-]
+  { path: '', redirectTo: 'rooms/1', pathMatch: 'full' },
+  {
+    path: 'rooms/:id',
+    component: DirectoryComponent,
+  },
+];
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)], 
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
