915f8cf06be18c5d4901c4740dcf5be5be8c769d
BLoveless <bloveless@talentpath.com>
2020-09-17 15:01:06 -0400

    component start route

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router'
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from '../app.component';
 import { DirectoryComponent } from '../directory/directory.component';
 
-
 const routes: Routes = [
-    {
-      path:"rooms/:id",
-      component: DirectoryComponent
-    }
-]
+  {
+    path: '/',
+    component: AppComponent,
+  },
+  {
+    path: 'rooms/:id',
+    component: DirectoryComponent,
+  },
+];
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)], 
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
