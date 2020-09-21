73169edaad61f10bfd83e9baa7ea669da0fdcf7b
Neely Mann <NMann@talentpath.com>
2020-09-21 06:16:01 -0400

    minus the API keys

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,12 +36,15 @@ import { QuickInfoComponent } from './listingInfo/quick-info/quick-info.componen
 import { BioBlurbInfoComponent } from './listingInfo/bio-blurb-info/bio-blurb-info.component';
 import { SleepingArrangementsInfoComponent } from './listingInfo/sleeping-arrangements-info/sleeping-arrangements-info.component';
 import { AmenitiesInfoComponent } from './listingInfo/amenities-info/amenities-info.component';
-import { LocationComponent } from './location/location.component';
+import { LocationComponent } from './location/location/location.component';
+import { MapComponent } from './location/map/map.component';
+import { PointsComponent } from './location/points/points.component';
+import { CityComponent } from './location/city/city.component';
 import { ReviewsComponent } from './Reviews/reviews.component';
 import { ReviewsService } from './services/reviews.service';
 import { DirectoryComponent } from './directory/directory.component';
-import { FooterComponent } from './footer/footer.component'
-
+import { FooterComponent } from './footer/footer.component';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
   declarations: [
@@ -65,6 +68,9 @@ import { FooterComponent } from './footer/footer.component'
     MptsComponentComponent,
     CardMptsComponent,
     LocationComponent,
+    MapComponent,
+    PointsComponent,
+    CityComponent,
     ListingInfoContainerComponent,
     TitleInfoComponent,
     StayDescriptorComponent,
@@ -75,9 +81,15 @@ import { FooterComponent } from './footer/footer.component'
     HostcohostComponent,
     DirectoryComponent,
     ReviewsComponent,
-    FooterComponent
+    FooterComponent,
   ],
-  imports: [BrowserModule, RouterModule, HttpClientModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    HttpClientModule,
+    AppRoutingModule,
+    GoogleMapsModule,
+  ],
 
   providers: [
     HostDataService,
