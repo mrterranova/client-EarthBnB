49e7f8ada3a201423f0aaae3118a75b3c3d3b22b
Michal Terranova <michalterranova@mail.com>
2020-09-22 16:24:27 -0400

    Changes to routing titlelisting (#124)
    Co-authored-by: Michal Terranova <MTerranova@talentpath.com>

--- src/app/listingInfo/sleeping-arrangements-info/sleeping-arrangements-info.component.ts
+++ src/app/listingInfo/sleeping-arrangements-info/sleeping-arrangements-info.component.ts
@@ -1,17 +1,43 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Data } from '@angular/router';
+import { TitlelistingService } from '../titlelisting-services/titlelisting.service';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-sleeping-arrangements-info',
   templateUrl: './sleeping-arrangements-info.component.html',
   styleUrls: ['./sleeping-arrangements-info.component.css']
 })
 export class SleepingArrangementsInfoComponent implements OnInit {
-  // tslint:disable-next-line: no-input-rename
-  @Input('sleepingarrdata') sleepingArrData: Data;
+  //global variables
+  private location; 
+  private sleep;
+  public sleeparr=[];
 
-  constructor() { }
-
+  constructor(private tlService: TitlelistingService, private route: ActivatedRoute) { }
   ngOnInit(): void {
+    this.getLoc(this.route.snapshot.params.id);
+  }
+
+  getLoc(id: number){
+    this.tlService.getLoc(id).subscribe(data => {this.location = data; this.getSleep();});
+    
+  }
+
+  getSleep(){
+    this.tlService.getSleep().subscribe(data => {
+      console.log("sleep", data)
+      this.sleep = data;
+      this.pushSleep(data)
+    })
+  }
+
+  pushSleep( arrangements ){
+    arrangements.map( arrange => {
+      if (arrange.location == this.location.id){
+        console.log("arrange", arrange)
+        this.sleeparr.push(arrange)
+      }
+    })
   }
 
 }
