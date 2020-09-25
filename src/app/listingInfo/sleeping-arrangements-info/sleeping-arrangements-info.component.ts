20e104bc3988ff76c9ecb35ba09e04142614156c
Michal Terranova <MTerranova@talentpath.com>
2020-09-25 08:05:18 -0400

    edits on title listing component

--- src/app/listingInfo/sleeping-arrangements-info/sleeping-arrangements-info.component.ts
+++ src/app/listingInfo/sleeping-arrangements-info/sleeping-arrangements-info.component.ts
@@ -25,10 +25,6 @@ export class SleepingArrangementsInfoComponent implements OnInit {
 
   getSleep(){
     this.tlService.getSleep().subscribe(data => {
-<<<<<<< HEAD
-=======
-      console.log("sleep", data)
->>>>>>> 1e6ec4eda69e2ef39bc46c419b6786811e248c28
       this.sleep = data;
       this.pushSleep(data)
     })
@@ -37,10 +33,6 @@ export class SleepingArrangementsInfoComponent implements OnInit {
   pushSleep( arrangements ){
     arrangements.map( arrange => {
       if (arrange.location == this.location.id){
-<<<<<<< HEAD
-=======
-        console.log("arrange", arrange)
->>>>>>> 1e6ec4eda69e2ef39bc46c419b6786811e248c28
         this.sleeparr.push(arrange)
       }
     })
